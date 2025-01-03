cb40ec24478850cdef418dd1adf65f525c2f399a
Marcin Bogusz <bogusz.marcin@icloud.com>
2025-01-03 22:16:03 +0100

    words and characters are counted

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    book_path = "books/frankenstein.txt"
+    
+    with open(book_path) as f:
         file_contents = f.read()
-        words = file_contents.split()
-        counter = len(words)
         
-    print(counter)
+    # count the words
+    words = file_contents.split()
+    words_count = len(words)
+    
+    # change the alpha characters to lower case and count    
+    lower_case = file_contents.lower()
+    characters = {}
+    for character in lower_case:
+        if character in characters:
+            characters[character] += 1
+        elif character.isalpha():
+            characters[character] = 1
+        
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{words_count} words found in the document")
+    print()
+    
+    for item in characters:
+        print(f"The {item} character was found {characters[item]} times")
+
+    print("--- End report ---")
 
 main()
